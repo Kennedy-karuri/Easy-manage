2fbe9a7edd644d843d55d6ce3a4b776fdb8e0418
Kennedy-karuri <kennkaruri99@gmail.com>
2023-03-07 16:37:32 +0300

    created landing page

--- wp-content/themes/easy-manage/landing-page.php
+++ wp-content/themes/easy-manage/landing-page.php
@@ -5,6 +5,259 @@
 ?>
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="wp-content/themes/easy-manage/assets/css/custom.css">
+    <title>Easy-manage</title>
+</head>
+<body>
+   
+<div class="homepage">
+    <div class="homepage-heading">
+        <div class="heading2">
+            <div class="brand">
+                <li class="list">EASY-MANAGE</li>
+            </div>
+            <div class="links">
+                <li class="list"><a href="#">Home</a></li>
+                <li class="list"><a href="#">About-Us</a></li>
+                <li class="list"><a href="#">Services</a></li>
+                <li class="list"><a href="#">Contact-Us</a></li>
+
+            </div>
+            
+        </div>
+        <div class="login">
+          <button class="login">LOGIN</button>
+           
+        </div>
+    </div>
+
+    <div class="homepage-content">
+        <h1>ASSIGNING PROJECT AND MANAGING TEAMS MADE EASIER</h1>
+        <div class="button">
+            <button class="start">GET STARTED</button>
+           
+        </div>
+    </div>
+</div>
+
+<div class="third-page">
+        <div class="third-header">
+            <h1>SERVICES</h1>
+            <p>The following services are offered at Easy-Manage.</p>
+        </div>
+        <div class="cards">
+            <div class="cards2">
+                <div class="cards-data">
+                    <img src="wp-content/themes/easy-manage/assets/img/manage.png">
+                    <h3>Project Management</h3>
+                    <p>Easily manage your projects.Save time and resources on our system.</p>
+                </div>
+
+                <div class="cards-data">
+                    <img src="wp-content/themes/easy-manage/assets/img/User.png">
+                    <h3>Manage Users</h3>
+                    <p>Manage users of your System.You can view the whole of your workforce from our application</p>
+                   
+                </div>
+
+                <div class="cards-data">
+                    <img src="wp-content/themes/easy-manage/assets/img/puser.png">
+                    <h3>Assign Projects</h3>
+                    <p>Assign projects to your different members.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="fourth-page">
+
+    <div class="fourth-header">
+            <h1>CONTACTS</h1>
+            <p>Incase of any comment or enquiry reach out to us through easymanage@admin.com or fill out the form below.</p>
+
+            <form>
+  <!-- 2 column grid layout with text inputs for the first and last names -->
+  <div class="row mb-4">
+    <div class="col">
+      <div class="form-outline">
+      <label class="form-label" for="form3Example1">First name</label>
+        <input type="text" id="form3Example1" class="form-control"  placeholder="input your First Name..." />
+        
+      </div>
+    </div>
+    <div class="col">
+      <div class="form-outline">
+      <label class="form-label" for="form3Example2">Last name</label>
+        <input type="text" id="form3Example2" class="form-control"  placeholder="input your second Name..." />
+        
+      </div>
+    </div>
+  </div>
+
+  <!-- Email input -->
+  <div class="row mb-4">
+    <div class="col">
+      <div class="form-outline">
+      <label class="form-label" for="form3Example1">Subject Matter</label>
+        <input type="text" id="form3Example1" class="form-control" placeholder="input your subject topic..."/>
+        
+      </div>
+    </div>
+
+    <!--Telephone -->
+    <div class="col">
+      <div class="form-outline">
+      <label class="form-label" for="form3Example2">Telephone Number</label>
+        <input type="text" id="form3Example2" class="form-control"  placeholder="input your telephone number..." />
+       
+      </div>
+    </div>
+  </div>
+
+
+   <!-- Message input -->
+   <div class="form-outline mb-4">
+   <label class="form-label" for="form4Example3"  placeholder="input your message...">Message</label>
+    <textarea class="form-control" id="form4Example3" rows="4"></textarea>
+   
+  </div>
+  <!-- Submit button -->
+  <button type="submit" class="btn btn-primary btn-block mb-4">Send</button>
+
+ 
+</form>
+
+        </div>
+
+    </div>
+
+<div class="container my-5">
+
+  <!-- Footer -->
+  <footer
+          class="text-center text-lg-start text-white"
+          style="background-color:rgb(48, 89, 218)"
+          >
+          <br>
+    <!-- Section: Social media -->
+    <section
+             class="d-flex justify-content-between p-4"
+             style="background-color: rgba(0, 0, 0, 0.2)"
+             >
+      <!-- Left -->
+      <div class="me-5">
+        <span>Get connected with us on social networks:</span>
+      </div>
+      <!-- Left -->
+
+      <!-- Right -->
+      <div>
+        <a href="" class="text-white me-4">
+          <img src="wp-content/themes/easy-manage/assets/img/linkedin.png" alt="linkedIn" style="height:50px;width:50px;">
+        </a>
+        <a href="" class="text-white me-4">
+        <img src="wp-content/themes/easy-manage/assets/img/twitter.png" alt="twitter" style="height:50px;width:50px;">      
+        </a>
+        <a href="" class="text-white me-4">
+        <img src="wp-content/themes/easy-manage/assets/img/facebook.png" alt="facebook" style="height:50px;width:50px;">      
+        </a>
+        <a href="" class="text-white me-4">
+        <img src="wp-content/themes/easy-manage/assets/img/insta.png" alt="instagram" style="height:50px;width:50px;">       
+       </a>
+      
+
+      </div>
+      <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold">EASY-MANAGE</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+             Assigning projects and Managing teams made Easier
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Useful links</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+              <a href="#!" class="text-white">HOME</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">JOIN-US</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">CONTACTS-US</a>
+            </p>
+            <p>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Contact</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p><img src="wp-content/themes/easy-manage/assets/img/phone.png" alt="" style="height:50px;width:50px;"> +254729429104</p>
+            <p><img src="wp-content/themes/easy-manage/assets/img/mail.png" alt="" style="height:50px;width:50px;"> easymanage@gmail.com</p>
+            <p><img src="wp-content/themes/easy-manage/assets/img/location.png" alt="" style="height:50px;width:50px;"> KDS,PLAZA NYERI</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div
+         class="text-center p-3"
+         style="background-color: rgba(0, 0, 0, 0.2)"
+         >
+      © 2023 Copyright:
+      <a class="text-white" href="https://kennedy-karuri.github.io/"
+         >KENNEDY KARURI</a
+        >
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+
+</div>
+<!-- End of .container -->
+
+
+</body>
+</html>
 
 
 
